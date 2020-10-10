c59d793b0a1ea01c90c119db93ff68132a440ad7
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2020-10-10 16:56:42 +0600

    Create template.py

---
+++ template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# pip3 install xlutils
+import xlrd
+import xlwt
+from xlutils.copy import copy
+import os
+
+def saveData(dataset, output_file):
+    output_file = "{}.xls".format(output_file) # this will be xls file name
+    fieldnames = ["Header 1", "Header 2", "Header 3", "Header 4"] # change your header list here
+    wb = xlwt.Workbook(encoding="UTF-8")
+    ws = wb.add_sheet("Sheet 1")
+    if os.path.exists(output_file):
+        # old file, get last untouched row and start writing data
+        rb = xlrd.open_workbook(output_file,formatting_info=True)
+        sht = rb.sheet_by_index(0)
+        lastRows = sht.nrows
+        wb = copy(rb)
+        sheet = wb.get_sheet(0)
+        pos = 0
+        for data in dataset:
+            sheet.write(lastRows, pos, data)
+            pos += 1
+        wb.save(output_file)
+    else:
+        # new file, create the headers
+        pos = 0
+        for fieldname in fieldnames:
+            ws.write(0, pos, fieldname)
+            pos += 1
+        # now saving first row of data
+        pos = 0
+        for data in dataset:
+            ws.write(1, pos, data)
+            pos += 1
+        wb.save(output_file)
+
+if __name__ == "__main__":
+    data = ["Cell data 1", "Cell data 2", "Cell data 3", "Cell data 4"]
+    saveData(data, "my_sheet")
